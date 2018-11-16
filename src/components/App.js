0f89ad80edbb11fb8667046f3ebc784791ecbd0f
nishikawa.yuto <jazzmonmee@gmail.com>
2018-11-16 11:24:43 +0900

    Write VisibleTodoList(container)

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import Footer from './Footer';
+import AddTodo from '../containers/VisibleTodoList';
+import visibleTodoList from '../containers/AddTodo';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+);
 
 export default App;
