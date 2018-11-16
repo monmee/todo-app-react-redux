4a2a5f3d55401250f73c3afa774b732c7bc68347
nishikawa.yuto <jazzmonmee@gmail.com>
2018-11-16 09:58:27 +0900

    Varibale name change

---
+++ src/reducers/todos.js
@@ -0,0 +1,22 @@
+const todos = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          isCompleted: false
+        }
+      ]
+    case 'TOGGLE_TODO':
+      return state.map(todo => 
+        (todo.id === action.id) ? {...todo, completed: !todo.isCompleted}
+        : todo
+      )
+    default: 
+      return state;
+  }
+}
+
+export default todos;
